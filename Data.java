9047c9aa78f055343ac0b7717ba74eed46333b51
HaoXK <haoxuank@nycstudents.net>
2025-04-28 12:26:03 -0400

    Update Data.java

--- Data.java
+++ Data.java
@@ -1,1 +1,13 @@
+public class Data {
+    public static final int Max = (int) (Math.random() * Integer.MAX_VALUE);
+    private int[][] grid;
+
+    public void repopulate() {
+
+    }
+
+    public int countIncreasingCols() {
+        return 0;
+    }
+}
 
