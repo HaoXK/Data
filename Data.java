d0b620c8304f244fb9e22332256040e132eb8762
HaoXK <haoxuank@nycstudents.net>
2025-05-03 16:55:50 -0400

    Update Data.java

--- Data.java
+++ Data.java
@@ -2,12 +2,48 @@ public class Data {
     public static final int Max = (int) (Math.random() * Integer.MAX_VALUE);
     private int[][] grid;
 
-    public void repopulate() {
+    public Data(int rows, int columns) {
+        grid = new int[rows][columns];
+    }
 
+    public Data(int[][] arr) {
+        grid = arr;
+    }
+
+    public String toString() {
+        String s = "";
+        for (int[] row : grid) {
+            for (int col : row) {
+                s += col + " ";
+            }
+            s += "/n";
+        }
+        return s;
+    }
+
+    public void repopulate() {
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                int val = 0;
+                while (val % 10 != 0 || val % 100 == 0) {
+                    val = (int) (Math.random() * Max + 1);
+                }
+                grid[row][col] = val;
+            }
+        }
     }
 
     public int countIncreasingCols() {
-        return 0;
+        int count = 0;
+        for (int col = 0; col < grid[0].length; col++) {
+            int increasing = 1;
+            for (int row = 1; row < grid.length; row++) {
+                if (grid[row][col] >= grid[row - 1][col]) increasing++;
+            }
+            if (increasing == grid.length) count++;
+        }
+        return count;
     }
-}
 
+ 
+}
